dae54bf468b04c5d6bc8ea7a6dab97fa1c294846
MASTER\martinh <martinh@kentico.com>
2018-11-30 13:09:34 +0100

    KCL-759 - Geo-location, Widgets (Edit elsewhere) + content (disabled, preview URL)

--- custom-element.js
+++ custom-element.js
@@ -34,26 +34,54 @@ var CustomElementClass = function() {
     window.addEventListener('message', processMessage, true)
 
     return {
+        // Returns { value: string } in callback
         getValue: function (cb) {
             registerCallback('get-value-response', cb);
             sendMessage('get-value');
         },
+
+        // Returns { success: true } or { error: Exception } in callback when done
         setValue: function (value, cb) {
             registerCallback('set-value-response', cb);
-            sendMessage('set-value', { value });
+            sendMessage('set-value', {value});
         },
+
+        // Returns { value: string } in callback
         getElementValue: function (elementId, cb) {
             registerCallback('get-value-response', cb);
-            sendMessage('get-value', { elementId });
+            sendMessage('get-value', {elementId});
         },
+
+        // Returns {
+        //  projectId: Uuid;
+        //  itemId: ContentItemId; { itemId: Uuid, variantId: Uuid }
+        //  element: ICompiledContentItemElementData { elementId: Uuid, type: string, name: string; }
+        //  previewUrl: string | undefined;
+        //  disabled: boolean;
+        // }
+        // in callback
+        getContext: function (cb) {
+            var repetitiveCb = function (data) {
+                cb(data);
+                registerCallback('get-context-response', repetitiveCb)
+            };
+            registerCallback('get-context-response', repetitiveCb);
+            sendMessage('get-context');
+        },
+
+        // Returns { value: string } in callback
         getClientRect: function (cb) {
             registerCallback('get-client-rect-response', cb);
             sendMessage('get-client-rect');
         },
+
+        // Returns { success: true } in callback when done
         setSize: function (size, cb) {
             registerCallback('set-size-response', cb);
             sendMessage('set-size', size);
         },
+
+        // Returns { elementIds: Uuid[] } (changed element ids) in callback when elements change
         observeChanges: function (elementIds, cb) {
             var repetitiveCb = function (data) {
               cb(data);
